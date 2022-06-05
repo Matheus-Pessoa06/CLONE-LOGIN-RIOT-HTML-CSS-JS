4a75bf74176e0d69f92212ad1464a359ea4905cc
Matheus Pessoa <55144224+matheuspessoa2010@users.noreply.github.com>
2022-06-05 17:33:15 -0300

    Add files via upload

---
+++ tela lol/index.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll('.input')
+const button = document.querySelector('.login-button')
+
+
+const handleFocus = ({target}) =>{
+    const span = target.previousElementSibling
+    span.classList.add('span-active')    
+}
+const handleFocusout = ({target}) =>{
+    if(target.value == ''){
+        const span = target.previousElementSibling
+        span.classList.remove('span-active')   
+    } 
+}
+const handleChange = () =>{
+    const [username, password] = inputs
+    if(username.value && password.value.length >=8){
+        button.removeAttribute('disabled')
+    }else{
+        button.setAttribute('disabled', ' ')
+    }
+}
+
+//O for each executa a funcao para cada elemento do array
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusout));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
